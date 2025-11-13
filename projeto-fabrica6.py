874d6539f001ae51173ab66e645c5ad7cac1facc
Lucas Alves dos Santos <santoslucas22498@gmail.com>
2025-11-13 11:54:25 -0300

    Add files via upload

---
+++ projeto-fabrica6.py
@@ -0,0 +1,7 @@
+def somarimposto(valor, taxa):
+    valor_final = valor + (valor *(taxa/100))
+    return valor_final
+
+valor =  float(input("digite o valor  "))
+taxa  =  float(input("digite o valor da taxa "))
+print (somarimposto(valor,taxa))
